918f77aac10f89e9962b579973169dfa2d30545a
AlekseyOgorodnikov <ogorodnikovalexey@gmail.com>
2021-11-27 10:39:34 +0400

    last change

--- src/App.js
+++ src/App.js
@@ -10,10 +10,31 @@ const axiosGitHubGraphQL = axios.create({
   },
 });
 
+const state = {
+  path: 'the-road-to-learn-react/the-road-to-learn-react',
+};
+
+function onChange(event) {}
+
+function onSubmit(event) {
+  event.preventDefault();
+}
+
 function App() {
   return (
     <div className="App">
       <h1>{title}</h1>
+      <form onSubmit={onSubmit}>
+        <label htmlFor="url">Show open issues for github</label>
+        <input
+          type="text"
+          id="url"
+          style={{ width: '300px' }}
+          onChange={onChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+      <hr />
     </div>
   );
 }
